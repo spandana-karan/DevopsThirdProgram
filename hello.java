0745542520b57b0a75765c18363b9b42509f66b0
Spandana <koora.spandana@vbithyd.ac.in>
2026-07-26 21:21:12 +0530

    Updated welcome message

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World! to hell DevOps");
+        System.out.println("welcome to github");
     }
 }
